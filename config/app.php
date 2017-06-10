74c5b692f7ba30372e329d434dbca81f4a840519
jxy <848280118@qq.com>
2017-06-10 20:50:18 +0800

    fix

--- config/app.php
+++ config/app.php
@@ -145,7 +145,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-
+        EricMakesStuff\ServerMonitor\ServerMonitorServiceProvider::class,
     ],
 
     /*
