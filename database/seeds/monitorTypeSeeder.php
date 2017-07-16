e91e51453fd259134a6e0c42717340d29644d7f9
jixinyu <848280118@qq.com>
2017-07-16 19:34:35 +0800

    add-seeder

---
+++ database/seeds/monitorTypeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class monitorTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('monitor_types')->insert([
+            ['name' => 'MySQL', 'default_host' => '127.0.0.1', 'default_port' => '3306','default_timeout'=>'2','default_times'=>'3'],
+            ['name' => 'Http', 'default_host' => '127.0.0.1', 'default_port' => '80','default_timeout'=>'2','default_times'=>'3'],
+            ['name' => 'Redis', 'default_host' => '127.0.0.1', 'default_port' => '6379','default_timeout'=>'2','default_times'=>'3'],
+            ['name' => 'SSH', 'default_host' => '127.0.0.1', 'default_port' => '22','default_timeout'=>'2','default_times'=>'3'],
+            ['name' => 'Custom', 'default_host' => '127.0.0.1', 'default_port' => '0','default_timeout'=>'2','default_times'=>'3'],
+        ]);
+    }
+}
