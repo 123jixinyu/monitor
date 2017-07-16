ea83ff35fcb06716ce17fa853eee605194ff25fa
jixinyu <848280118@qq.com>
2017-07-16 11:31:11 +0800

    fix

---
+++ resources/views/system/index.blade.php
@@ -0,0 +1,9 @@
+@extends('adminlte::page')
+@section('css')
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet"/>
+@endsection
+@section('content')
+    xxx
+@endsection
+@section('js')
+@endsection
